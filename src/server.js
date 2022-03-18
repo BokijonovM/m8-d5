06bd8a86f33218376f556bf491b22ad9c02a0d58
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-18 13:48:45 +0500

    files added

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import {
+  unauthorizedHandler,
+  forbiddenHandler,
+  catchAllHandler,
+} from "./error-handlers.js";
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
+import passport from "passport";
+
+const server = express();
+const port = process.env.PORT || 3001;
+
+server.use(cors());
+server.use(express.json());
+server.use(passport.initialize());
+
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(catchAllHandler);
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!");
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log("Server runnning on port: ", port);
+  });
+});
