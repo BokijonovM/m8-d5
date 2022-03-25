354fd5e5c78525c566770e22b0251011d3283cf0
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-25 15:20:36 +0100

    f

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,160 +1,168 @@
 import { server } from "../server";
 import supertest from "supertest";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import mongoose from "mongoose";
+process.env.TS_NODE_DEV && require("dotenv").config();
 
-dotenv.config()
-
-
-
-interface IUser  {
-    name : string,
-    surname : string,
-    email : string,
-    password : string,
-    role : string
+interface IUser {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+  role: string;
 }
-let token : string
-let wrongToken : string
+let token: string;
+let wrongToken: string;
 
 describe("Testing the chat endpoints", () => {
+  const client = supertest(server);
+  const MONGO_URL_TEST = process.env.MONGO_URL_TEST!;
 
-    const client = supertest(server)
-const MONGO_URL_TEST = process.env.MONGO_URL_TEST!
+  beforeAll((done) => {
+    // console.log(process.env.MONGO_URL_TEST)
+    mongoose.connect(MONGO_URL_TEST).then(() => {
+      console.log("Connected to Mongo DB in test...");
+      done();
+    });
+  });
 
-    beforeAll(done => {
-        // console.log(process.env.MONGO_URL_TEST)
-        mongoose.connect(MONGO_URL_TEST)
-            .then(() => {
-                console.log("Connected to Mongo DB in test...")
-                done()
-            })
-    })
+  it("should work", () => {
+    expect(true).toBe(true);
+  });
 
+  const newUser = {
+    name: "Test Name",
+    surname: "Test Surname",
+    email: "test@email.com",
+    password: "Testpassword",
+    role: "user",
+  };
 
-    it("should work", () => {
-        expect(true).toBe(true);
-    })
+  const incompleteUser = {
+    name: "Test Name",
+    surname: "Test Surname",
+    role: "user",
+  };
 
-   const newUser = {
-    name : "Test Name",
-    surname : "Test Surname",
-    email : "test@email.com",
-    password : "Testpassword",
-    role : "user"
-   }
-
-
-   const incompleteUser = {
-    name : "Test Name",
-    surname : "Test Surname",
-    role : "user"
-   }
-
-
-   /******************* Posting new user ******************/
-   it("should create a new user using POST /register", async () => {
-        const response = await client.post('/register').send(newUser)
-        expect(response.status).toBe(201)
-        token = response.body.token
-        // expect(response.body.token).toBeDefined()
-    })
-    
-    
-    /******************* Posting incomplete user ******************/
-    it(" missing data should not create a new user using POST /register", async () => {
-        const response = await client.post('/register').send(incompleteUser)
-        
-        expect(response.status).toBe(401)
-    })
-    
-    const loginUser = {
-        email : "test@email.com",
-        password : "Testpassword"
-    }
-    
-    const wrongloginUser = {
-        email : "wrong@email.com",
-        password : "wrongpassword"
-    }
-    
-
-      /******************* login  ******************/
-   it("should create a new user using POST /register", async () => {
-    const response = await client.post('/register').send(loginUser)
-    
-    expect(response.status).toBe(201)
-    token = response.body.token
-    wrongToken = "wrong" + token
+  /******************* Posting new user ******************/
+  it("should create a new user using POST /register", async () => {
+    const response = await client.post("/register").send(newUser);
+    expect(response.status).toBe(201);
+    token = response.body.token;
     // expect(response.body.token).toBeDefined()
- })
- 
- 
- /******************* login   ******************/
- it(" missing data should not create a new user using POST /register", async () => {
-     const response = await client.post('/login').send()
-     
-     expect(response.status).toBe(401)
- })
+  });
 
+  /******************* Posting incomplete user ******************/
+  it(" missing data should not create a new user using POST /register", async () => {
+    const response = await client.post("/register").send(incompleteUser);
 
- /******************* get all by admin ******************/
- it("should create a new user using get /", async () => {
-    const response = await client.get('/me').set("authorization", token)
-    
-    expect(response.status).toBe(201)
-    })
-    
-    
-    /******************* cannot get  by user ******************/
-    it(" missing data should not create a new user using POST /register", async () => {
-        const response = await client.get('/').set("authorization", wrongToken)
-        expect(response.status).toBe(401)
-    })
+    expect(response.status).toBe(401);
+  });
 
- /******************* get me ******************/
- it("should create a new user using POST /me", async () => {
- const response = await client.put('/me').set("authorization", token)
- 
- expect(response.status).toBe(201)
- })
- 
- 
- /******************* cannot get  me ******************/
- it(" missing data should not create a new user using POST /register", async () => {
-     const response = await client.put('/me').set("authorization", wrongToken)
-     expect(response.status).toBe(401)
- })
+  const loginUser = {
+    email: "test@email.com",
+    password: "Testpassword",
+  };
 
- const editUser = {
-    name : "Edited Name",
-    surname : "Edited Surname",
-    role : "user"
-   }
+  const wrongloginUser = {
+    email: "wrong@email.com",
+    password: "wrongpassword",
+  };
 
-    /******************* Edit users ******************/
-    it("should create a new user using POST /me", async () => {
-    const response = await client.put('/me').send(editUser).set("authorization", token)
-    
-    expect(response.status).toBe(201)
-    })
-    
-    
-    /******************* edit  users ******************/
-    it(" missing data should not create a new user using POST /register", async () => {
-        const response = await client.put('/me').send().set("authorization", wrongToken)
-        
-        expect(response.status).toBe(401)
-    })
+  /******************* login  ******************/
+  it("should create a new user using POST /register", async () => {
+    const response = await client.post("/register").send(loginUser);
 
+    expect(response.status).toBe(201);
+    token = response.body.token;
+    wrongToken = "wrong" + token;
+    // expect(response.body.token).toBeDefined()
+  });
 
+  /******************* login   ******************/
+  it(" missing data should not create a new user using POST /register", async () => {
+    const response = await client.post("/login").send();
 
-    afterAll(async() => {
-        await mongoose.connection.dropDatabase()
-        await mongoose.connection.close()
-        
-            console.log("Dropped database and closed connection")
-           
-        })
+    expect(response.status).toBe(401);
+  });
 
-})
+  /******************* get all by admin ******************/
+  it("should create a new user using get /", async () => {
+    const response = await client.get("/me").set("authorization", token);
+
+    expect(response.status).toBe(201);
+  });
+
+  /******************* cannot get  by user ******************/
+  it(" missing data should not create a new user using POST /register", async () => {
+    const response = await client.get("/").set("authorization", wrongToken);
+    expect(response.status).toBe(401);
+  });
+
+  /******************* get me ******************/
+  it("should create a new user using POST /me", async () => {
+    const response = await client.put("/me").set("authorization", token);
+
+    expect(response.status).toBe(201);
+  });
+
+  /******************* cannot get  me ******************/
+  it(" missing data should not create a new user using POST /register", async () => {
+    const response = await client.put("/me").set("authorization", wrongToken);
+    expect(response.status).toBe(401);
+  });
+
+  const editUser = {
+    name: "Edited Name",
+    surname: "Edited Surname",
+    role: "user",
+  };
+
+  /******************* Edit users ******************/
+  it("should create a new user using POST /me", async () => {
+    const response = await client
+      .put("/me")
+      .send(editUser)
+      .set("authorization", token);
+
+    expect(response.status).toBe(201);
+  });
+
+  /******************* edit  users ******************/
+  it(" missing data should not create a new user using POST /register", async () => {
+    const response = await client
+      .put("/me")
+      .send()
+      .set("authorization", wrongToken);
+
+    expect(response.status).toBe(401);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+
+    console.log("Dropped database and closed connection");
+  });
+});
+
+describe("Testing the acc endpoints", () => {
+  beforeAll((done) => {
+    // console.log(process.env.MONGO_URL_TEST)
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+      console.log("Connected to Mongo DB in test...");
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    mongoose.connection
+      .dropDatabase()
+      .then(() => {
+        return mongoose.connection.close();
+      })
+      .then(() => {
+        console.log("Dropped database and closed connection");
+        done();
+      });
+  });
+});
